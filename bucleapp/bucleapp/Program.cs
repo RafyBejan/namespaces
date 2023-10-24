34f8eb2f08a73902df43f65fed04c23e83c99e31
bejan <bejan@RAFAEL12>
2023-10-24 17:41:49 +0300

    initial project

---
+++ bucleapp/bucleapp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace bucleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { const string username = "Andrei";
+            const string password = "oradea23";
+            string userInput, passwordInput;
+            while (true)
+            {
+                Console.Write("Introduceti username:");
+                userInput = Console.ReadLine();
+
+                Console.Write("Introduceti parola:");
+                passwordInput = Console.ReadLine();
+
+                if (userInput == username && passwordInput == password)
+                { Console.Write("Bine ai venit,Andrei");
+                    break;
+                }
+                else
+                { Console.Write("Autentificare esuata"); }
+                       
+            }
+        }
+    }
+}
---
+++ namespacess/ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+       
+    }
+}
